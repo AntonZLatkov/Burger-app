77badc2ee0810e6da34b66a74282f975d3ed94ab
Anton Vasilev <antonzlatkov@pop-os.localdomain>
2021-01-10 08:19:37 +0200

    Optimizations when component should not re-render

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,35 +1,36 @@
 import Aux from '../../../hoc/Aux';
-import Button from '../../UI/Button/Button';
+import Button from '../../UI/Button/Button'
 
 const orderSummary = (props) => {
+
   const ingredientSummary = Object.keys(props.ingredients)
     .map(igKey => {
       return (
         <li key={igKey}>
-          <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+          <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingredients[igKey]}
         </li>
       )
     })
-  
+
   return (
     <Aux>
       <h3>Your Order</h3>
       <p>A delicious burger with...</p>
       <ul>
-        { ingredientSummary }
+        {ingredientSummary}
       </ul>
       <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
       <p>Continue to Checkout?</p>
-      <Button 
-        clicked={props.purchaseCancelled} 
+      <Button
+        clicked={props.purchaseCancelled}
         btnType='Danger'
       >Cancel</Button>
-      <Button 
-        clicked={props.purchaseContinued} 
+      <Button
+        clicked={props.purchaseContinued}
         btnType='Success'
       >Continue</Button>
     </Aux>
   );
 }
- 
+
 export default orderSummary;
--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,25 +1,33 @@
 import classes from "./Modal.module.css";
 import Backdrop from '../Backdrop/Backdrop';
 import Aux from '../../../hoc/Aux';
+import React, { Component } from 'react';
 
-const modal = (props) => {
-  return (
-    <Aux>
-      <Backdrop
-        clicked={props.modalClosed}
-        show={props.show}
-      />
-      <div
-        className={classes.Modal}
-        style={{
-          transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-          opacity: props.show ? '1' : '0'
-        }}
-      >
-        { props.children }
-      </div>
-    </Aux>
-  );
+class Modal extends Component {
+  shouldComponentUpdate(nextProps, nextState) {
+    return nextProps.show !== this.props.show;
+  }
+
+  render() {
+    return (
+      <Aux>
+        <Backdrop
+          clicked={this.props.modalClosed}
+          show={this.props.show}
+        />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0'
+          }}
+        >
+          { this.props.children }
+        </div>
+      </Aux>
+    );
+  }
+  
 }
  
-export default modal;
+export default Modal;
